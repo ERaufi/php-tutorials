4c212126ce530d4a698ed7bdca398e6e91c12e96
ERaufi <edrisraufi@gmail.com>
2025-10-03 02:18:48 +0300

    Add initial PHP files for comments, echo, and variables examples

---
+++ comment.php
@@ -0,0 +1,14 @@
+<?php
+// this is a single line comment
+echo "hello, PHP"; # this is comment
+
+/* 
+thisf
+ asdfadsf
+ asfd asdf
+ asdf asdf 
+ asdf 
+ */
+
+ echo "this is after comment"
+?>
---
+++ variables.php
@@ -0,0 +1,10 @@
+<?php
+$text = "Hello";   // String
+$number = 100;     // Integer
+$price = 19.99;    // Float
+$isActive = true;  // Boolean
+$name = "my name";
+$age = 25;
+echo $name; // my name
+echo $age;  // 25
+?>

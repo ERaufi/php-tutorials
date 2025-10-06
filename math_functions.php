a01cb44582305e4915f16e0115b5f87a1d6a0f94
ERaufi <edrisraufi@gmail.com>
2025-10-07 01:32:42 +0300

    Add math functions PHP tutorial: include examples for abs, round, max, min, and random number generation

---
+++ math_functions.php
@@ -0,0 +1,37 @@
+<?php
+
+// abs
+// echo abs(-20)
+
+// echo 5**3;
+// echo pow(5,3);
+
+// echo sqrt(25)
+
+
+// round
+// ceil
+// floor
+
+// echo round(3.5)
+// echo ceil(3.1)
+// echo floor(3.2)
+
+// max
+// min
+
+// echo max(5,10,2,8)
+// echo min(5,10,2,8)
+
+
+// generate random number
+// echo rand();
+// echo rand(1,100)
+
+
+$x=12345.6789;
+
+// echo($x);
+echo number_format($x,4,'*','-')
+
+?>
